5b5c467736936c5fecd6a7553836e820a52a6d85
Max Pirotais-Wilton <max.pirotais-wilton@integration.works>
2024-06-10 14:36:36 +1200

    renamed use case class to PersistPersonUseCase

--- person_subscriber/src/main/java/com/max_pw_iw/person_subscriber/application/port/in/PersistPersonUseCase.java
+++ person_subscriber/src/main/java/com/max_pw_iw/person_subscriber/application/port/in/PersistPersonUseCase.java
@@ -2,7 +2,7 @@ package com.max_pw_iw.person_subscriber.application.port.in;
 
 import com.max_pw_iw.person_subscriber.application.domain.model.Person;
 
-public interface SendPersonUseCase {
+public interface PersistPersonUseCase {
     
     void SendPerson(Person person);
 
